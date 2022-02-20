ef8021702cbec30e5ee74667d0189659eb7f5c46
kaperrdenden <weather2sogood@gmail.com>
2022-02-21 02:51:25 +0900

    classNames.bind() 사용하기 전 css module로 클래스 적용하기

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import {MdCheckBox,MdCheckBoxOutlineBlank } from 'react-icons/md'
+import styles from './CheckBox.module.css';
+import classNames from 'classnames/bind';
+
+
 function CheckBox({children, checked, ...rest}){
     return(
-        <div>
+        <div className={styles.checkbox}>
             <label>
                 <input type="checkbox" checked={checked} {...rest} />
-                <div>{checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank/>}</div>
+                <div className={styles.icon}
+                    >{checked ? <MdCheckBox  className={styles.checked}/> 
+                                : <MdCheckBoxOutlineBlank/>}
+                </div>
             </label>
             <span>{children}</span>
             dasd
