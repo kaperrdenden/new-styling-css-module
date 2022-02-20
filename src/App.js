d7740d7457eab0fa7ab5f7d9f2b79085059ab5d8
kaperrdenden <weather2sogood@gmail.com>
2022-02-21 02:30:09 +0900

    체크박스 구현

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import CheckBox from './components/CheckBox';
+import { useState } from 'react';
+
+function App() {
+
+  const [check, setCheck] = useState(false);
+  const onChange = e => {
+    setCheck(e.target.checked);
+  }
+  return (
+    <div className="App">
+      <CheckBox onChange={onChange} checked={check}>다음 약관에 모두 동의</CheckBox>
+        
+      <p>
+        <b>check: </b>
+        {check ? 'true' : 'false'}
+      </p>
+    </div>
+  );
+}
+
+export default App;
